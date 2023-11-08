da0deeece1ca402c7ad2a4f7c1061f55345e2b68
Joe Philip <joe.philip@hotmail.co.in>
2023-11-08 16:12:38 +0530

    added loggin in exception handler

--- root/utils/exceptions.py
+++ root/utils/exceptions.py
@@ -1,3 +1,5 @@
+from logging import error
+
 from rest_framework.exceptions import APIException
 from rest_framework.response import Response
 
@@ -7,4 +9,5 @@ from .utils import fail
 def exception_handler(exc, context):
     if isinstance(exc, APIException):
         return Response(fail(exc.detail), status=exc.status_code if exc.status_code else 500)
-    return Response(fail(str(exc)), status=500)
+    error(exception := str(exc))
+    return Response(fail(exception), status=500)
