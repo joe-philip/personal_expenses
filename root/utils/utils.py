784ac94d620ae7dbef42112ae606812118ec07fb
Joe Philip <joe.philip@hotmail.co.in>
2023-11-08 15:10:10 +0530

    defined success() and fail()

--- root/utils/utils.py
+++ root/utils/utils.py
@@ -1,7 +1,19 @@
 from datetime import datetime
+from typing import Any
 
 from django.utils.text import slugify
 
 
 def slug_generate(key: str = 'slug') -> str:
     return slugify(f'{key}-{datetime.now().timestamp()}')
+
+
+def success(data: Any = None) -> dict:
+    response = {'status': True, 'message': 'success'}
+    if data is not None:
+        response['data'] = data
+    return response
+
+
+def fail(error: Any) -> dict:
+    return {'status': False, 'message': 'fail', 'error': error}
