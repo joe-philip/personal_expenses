730b25eb281f8b95df19314eb8856071636ff615
Joe Philip <joe.philip@hotmail.co.in>
2023-11-08 20:07:38 +0530

    replaced APIView class

--- main/views.py
+++ main/views.py
@@ -1,8 +1,8 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from root.utils.utils import success
+from root.utils.views.api_view import APIView
 
 # Create your views here.
 
