dfe125c9bf6886174a188d6553a917f7c6c92daa
Joe Philip <joe.philip@hotmail.co.in>
2023-11-08 20:02:45 +0530

    defined HasAPIKey permission

---
+++ root/utils/permissions/api_key.py
@@ -0,0 +1,16 @@
+from rest_framework.permissions import BasePermission
+from rest_framework.request import Request
+
+from main.models import APIKeyManager
+
+
+class HasAPIKey(BasePermission):
+    message = 'Invalid API Key'
+
+    def has_permission(self, request: Request, view) -> bool:
+        print(request.authenticators, request.successful_authenticator)
+        if 'HTTP_X_API_KEY' in request.META:
+            key = request.META['HTTP_X_API_KEY']
+            if APIKeyManager().validate_key(key):
+                return True
+        return False
