6e0c16abef57e15f12ba83de3871462196173083
Joe Philip <joe.philip@hotmail.co.in>
2023-11-08 16:15:27 +0530

    defined HomeAPI

--- main/views.py
+++ main/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from root.utils.utils import success
 
 # Create your views here.
+
+
+class HomeAPI(APIView):
+    def get(self, request: Request) -> Response:
+        return Response(success())
