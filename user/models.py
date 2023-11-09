ac70d366ae73ce221e7096b56a9bbbb26605dec4
Joe Philip <joe.philip@hotmail.co.in>
2023-11-09 12:24:14 +0530

    defined UserProfilesModel

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+from main.fields import CommonFields
+from root.utils.choices import GenderChoices
+
 # Create your models here.
+
+
+class UserProfiles(CommonFields):
+    dob = models.DateField()
+    user = models.OneToOneField('main.User', on_delete=models.CASCADE)
+    gender = models.IntegerField(choices=GenderChoices.choices)
+    bio = models.TextField(blank=True)
+
+    class Meta:
+        db_table = 'user_profiles'
+        verbose_name = 'User profile'
+
+    def __str__(self) -> str: return self.user.first_name
