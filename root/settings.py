ca290963179631e923dbdea170e1c2b296df2258
Joe Philip <joe.philip@hotmail.co.in>
2023-11-09 12:00:52 +0530

    modified settings.py
    fixed error in INSTALLED_APPS

--- root/settings.py
+++ root/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
-    'user'
+    'user',
     'rest_framework',
     'rest_framework.authtoken'
 ]
