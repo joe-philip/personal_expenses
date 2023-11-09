1d1fa89d1afaf74a41064d36e2f5e5f88969b405
Joe Philip <joe.philip@hotmail.co.in>
2023-11-09 16:51:34 +0530

    removed CommonFields

--- user/models.py
+++ user/models.py
@@ -1,12 +1,17 @@
+from collections.abc import Iterable
+
 from django.db import models
 
-from main.fields import CommonFields
 from root.utils.choices import GenderChoices
+from root.utils.utils import slug_generate
 
 # Create your models here.
 
 
-class UserProfiles(CommonFields):
+class UserProfiles(models.Model):
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     dob = models.DateField()
     user = models.OneToOneField('main.User', on_delete=models.CASCADE)
     gender = models.IntegerField(choices=GenderChoices.choices)
@@ -18,3 +23,8 @@ class UserProfiles(CommonFields):
         verbose_name = 'User profile'
 
     def __str__(self) -> str: return self.user.first_name
+
+    def save(self, force_insert: bool = ..., force_update: bool = ..., using: str | None = ..., update_fields: Iterable[str] | None = ...) -> None:
+        if not self.slug:
+            self.slug = slug_generate()
+        return super().save(force_insert, force_update, using, update_fields)
