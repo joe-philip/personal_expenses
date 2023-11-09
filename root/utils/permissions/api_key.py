71607c20c9b335004cbb648b0d1d789cfa005346
Joe Philip <joe.philip@hotmail.co.in>
2023-11-09 16:58:29 +0530

    removed print statement

--- root/utils/permissions/api_key.py
+++ root/utils/permissions/api_key.py
@@ -8,7 +8,6 @@ class HasAPIKey(BasePermission):
     message = 'Invalid API Key'
 
     def has_permission(self, request: Request, view) -> bool:
-        print(request.authenticators, request.successful_authenticator)
         if 'HTTP_X_API_KEY' in request.META:
             key = request.META['HTTP_X_API_KEY']
             if APIKeyManager().validate_key(key):
